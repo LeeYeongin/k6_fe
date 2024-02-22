6bc0c2f04334280fc93a2cbcf0abe05c430eb031
LeeYeongin <youngin5757@gmail.com>
2024-02-22 12:53:13 +0900

    자바스크립트 함수

---
+++ 03/03.js
@@ -0,0 +1,11 @@
+function handleClick(n) {
+    // innerHTML : 속성(값). html문법도 가능
+    // 메시지 영역 가져오기 1
+    // document.getElementById("msgArea").innerHTML = "안녕하세요";
+
+    // 메시지 영역 가져오기 2
+    // document.querySelector("#msgArea").innerHTML = "<h2>버튼" + n + "이 눌러졌습니다.</h2>";
+
+    // 문자열과 변수 사용을 위해서 전체를 `로 감싸기. 변수는 ${}로 감싸기
+    document.querySelector("#msgArea").innerHTML = `<h2>버튼 ${n}이 눌러졌습니다.</h2>`;
+}
