e549fe50df34200a31fca7729b7e29cfb9935e83
LeeYeongin <youngin5757@gmail.com>
2024-02-26 17:35:27 +0900

    단위환산 txt영역 input이벤트 수정

--- 07/07.js
+++ 07/07.js
@@ -1,4 +1,9 @@
-const handleChange = (s1, s2, lb1, lb2) => {
+const handleChange = (s1, s2, lb1, lb2, t1, t2) => {
+    // txt값들 초기화
+    t1.value = '';
+    t2.value = '';
+    t1.focus();
+
     if(s1.value === "℃"){
         s2.value = "℉";
         lb1.innerHTML = "℃";
@@ -22,20 +27,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
     sel1.addEventListener("change", ()=>{
         console.log(sel1.value); // form의 값은 .value로 가져옴
-        handleChange(sel1, sel2, labels[0], labels[1]);
+        handleChange(sel1, sel2, labels[0], labels[1], txt1, txt2);
     })
  
     sel2.addEventListener("change", ()=>{
         console.log(sel2.value);
-        handleChange(sel2, sel1, labels[1], labels[0]);
+        handleChange(sel2, sel1, labels[1], labels[0], txt1, txt2);
     })
 
-    // txt1의 값이 변경되면 선택된 label값("℃", "℉"에 따라 계산하여 txt2에 출력)
-    txt1.addEventListener("change", () => {
+    // txt1의 값이 입력되면 선택된 label값("℃", "℉"에 따라 계산하여 txt2에 출력)
+    // input: 값이 바뀔때마다 바로 반영. change: enter 입력 후 반영
+    txt1.addEventListener("input", () => {
         console.log(txt1.value);
         if(sel1.value === "℃")
-            txt2.value = (txt1.value * (9/5)) + 32;
+            txt2.value = (parseFloat(txt1.value) * (9/5)) + 32;
         else
-            txt2.value = (txt1.value-32) * (5/9);
+            txt2.value = (parseFloat(txt1.value)-32) * (5/9);
     })
 })
