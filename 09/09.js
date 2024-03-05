154662a9639860f39f449cd6640eba731cf31a1e
LeeYeongin <youngin5757@gmail.com>
2024-03-05 12:28:50 +0900

    자료형 array, object

--- 09/09.js
+++ 09/09.js
@@ -9,10 +9,12 @@ console.log("실수: ", n2);
 console.log("문자열: ", s1);
 
 // 문자열 순회
-for(let i in s1){ // in을 사용하여 key값이 나옴
+console.log("문자열 for in")
+for(let i in s1){ // in을 사용하여 key(index)값이 나옴
     console.log("문자열: ", s1[i]);
 }
 
+console.log("문자열 for of")
 for(let c of s1){ // 해당 값을 가져옴
     console.log("문자열: ", c);
 }
@@ -23,7 +25,36 @@ let arr = ['🍎', 1, '🥕', 2, '🍌', 3,];
 console.log("배열: ", arr);
 console.log("배열의 요소 접근 arr[1]: ", arr[1]);
 
+// 배열 순회
+console.log("배열 for in")
+for(let i in arr){
+    console.log("배열: ", arr[i]);
+}
+
+console.log("배열 for of")
+for(let c of arr){
+    console.log("배열: ", c);
+}
+
 // 오브젝트
 let obj = {'🍎': 1, '🥕': 2, '🍌': 3,}; 
 console.log("오브젝트: ", obj);
 console.log("오브젝트 요소 접근 obj['🍎']: ", obj['🍎']);
+
+// 오브젝트 순회
+console.log("오브젝트 for in")
+for(let i in obj){
+    console.log("오브젝트: ", obj[i]);
+}
+
+console.log("오브젝트 for of")
+for(let c of Object.entries(obj)){ 
+    console.log("오브젝트: ", c); // [key, value]값을 쌍으로 동시에 가져옴
+    console.log("오브젝트 키: ", c[0]);
+    console.log("오브젝트 값: ", c[1]);
+}
+
+for(let [k,v] of Object.entries(obj)){  // 구조 분해
+    console.log("오브젝트 키: ", k);
+    console.log("오브젝트 값: ", v);
+}
