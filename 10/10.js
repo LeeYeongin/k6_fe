e37296d94645bd04845db0ce0fdc364509d23b3d
LeeYeongin <youngin5757@gmail.com>
2024-03-08 15:07:07 +0900

    로또생성기

--- 10/10.js
+++ 10/10.js
@@ -1,25 +1,52 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const nums = document.querySelectorAll("span");
-    const bt = document.querySelector("section button");
+// document.addEventListener("DOMContentLoaded", () => {
+//     const nums = document.querySelectorAll("span");
+//     const bt = document.querySelector("section button");
 
-    let n;
-    let arr = [];
+//     let n;
+//     let arr = [];
 
-    bt.addEventListener("click", ()=>{
-        arr = [];
-        for(let i = 0; i<7; i++){
-            n = Math.floor(Math.random() * 46) + 1;
-            while(arr.includes(n)){
-                n = Math.floor(Math.random() * 46) + 1;
-            }
-            arr.push(n);
-        }
+//     bt.addEventListener("click", ()=>{
+//         arr = [];
+//         for(let i = 0; i<7; i++){
+//             n = Math.floor(Math.random() * 46) + 1;
+//             while(arr.includes(n)){
+//                 n = Math.floor(Math.random() * 46) + 1;
+//             }
+//             arr.push(n);
+//         }
         
-        let idx = 0;
-        for(let num of nums){
-            num.innerHTML = arr[idx];
-            idx++;
-        }
+//         let idx = 0;
+//         for(let num of nums){
+//             num.innerHTML = arr[idx];
+//             idx++;
+//         }
         
+//     })
+// })  
+
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const bt = document.querySelector("button");
+    const numDisp =document.querySelector("#numDisp");
+
+    let nums = [];
+
+    bt.addEventListener("click", ()=> {
+        // 배열초기화
+        nums=[];
+
+        // 1~45까지 랜럼수가 중복되지 않게 7개 만들기
+        while(nums.length < 7){
+            let n = Math.floor(Math.random()*45)+1;
+            nums.push(n);
+        }
+        // console.log(nums);
+
+        let tags = '';
+        nums.map((v) => {
+            tags = tags + `<span class="sp${parseInt(v/10)}">${v}</span>`;
+            // console.log(tags);
+        })
+        numDisp.innerHTML = tags;
     })
-})  
+})
