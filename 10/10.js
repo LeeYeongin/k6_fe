79ba9d9cdc0037b9f8bb44406c5abf6af6e07799
LeeYeongin <youngin5757@gmail.com>
2024-03-08 16:20:46 +0900

    로또번호 생성기

--- 10/10.js
+++ 10/10.js
@@ -14,39 +14,49 @@
 //             }
 //             arr.push(n);
 //         }
-        
+
 //         let idx = 0;
 //         for(let num of nums){
 //             num.innerHTML = arr[idx];
 //             idx++;
 //         }
-        
+
 //     })
 // })  
 
 
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
     const bt = document.querySelector("button");
-    const numDisp =document.querySelector("#numDisp");
+    const numDisp = document.querySelector("#numDisp");
 
     let nums = [];
 
-    bt.addEventListener("click", ()=> {
+    bt.addEventListener("click", () => {
         // 배열초기화
-        nums=[];
+        nums = [];
 
         // 1~45까지 랜럼수가 중복되지 않게 7개 만들기
+        
         while(nums.length < 7){
             let n = Math.floor(Math.random()*45)+1;
-            nums.push(n);
+            if (!nums.includes(n))
+                nums.push(n);
         }
-        // console.log(nums);
-
-        let tags = '';
-        nums.map((v) => {
-            tags = tags + `<span class="sp${parseInt(v/10)}">${v}</span>`;
-            // console.log(tags);
-        })
-        numDisp.innerHTML = tags;
+        
+        // 코드 1
+        // let tags = '';
+        // nums.map((v, i) => {
+        //     tags = tags + `<span class="sp${parseInt(v/10)}">${v}</span>`;
+        //     if (i === 5)
+        //         tags = tags + `<span>+</span>`
+        //     // console.log(tags);
+        // })
+        // numDisp.innerHTML = tags;
+        
+        // 코드 2
+        nums = nums.map(v => `<span class="sp${parseInt(v/10)}">${v}</span>`);
+        nums.splice(6,0,'<span>+</span>');
+        numDisp.innerHTML = nums.toString().replaceAll(',',''); // 배열을 문자열로 방법 1
+        nums.join('');   // 배열을 문자열로 방법 2
     })
 })
